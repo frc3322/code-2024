fe3c72682ba9ce5ae0040cd81534b0ea5c40c9f9
IBuildRoboats <grayakry@gmail.com>
2024-01-10 16:51:19 -0500

    AdvantageKit configured

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "code-2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "db8555f3b14880ca2adb84b11c847da63eefcf13";
+  public static final String GIT_DATE = "2024-01-10 14:17:27 EST";
+  public static final String GIT_BRANCH = "AdvantageKit";
+  public static final String BUILD_DATE = "2024-01-10 16:45:25 EST";
+  public static final long BUILD_UNIX_TIME = 1704923125462L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -175,4 +175,19 @@ public final class Constants {
 
     public static final int neo550CurrentLimitAmps = 20;
   }
+
+  public static final class AdvantageKitConstants{
+    public static final Mode currentMode = Mode.REAL;
+
+    public static enum Mode {
+      /** Running on a real robot. */
+      REAL,
+
+      /** Running a physics simulator. */
+      SIM,
+
+      /** Replaying from a log file. */
+      REPLAY
+    }
+  }
 }
