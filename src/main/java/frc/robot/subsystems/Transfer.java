a9236f18d4950f6e184e88c57cb4b52b9fa209c5
DarkEden-Coding <313947@aaps.k12.mi.us>
2024-02-13 19:48:52 -0500

    Unused imports

--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -10,7 +10,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -18,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.CANIds;
-import frc.robot.Constants.DIOids;
 import frc.robot.Constants.NeoMotorConstants;
 import frc.robot.Constants.TransferConstants;
 import io.github.oblarg.oblog.Loggable;
