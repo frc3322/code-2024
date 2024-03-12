693d9e984e669b365210528fa88d6b607fe0ec39
Caitlyn <cmtrievel@icloud.com>
2024-03-11 21:02:02 -0400

    added amp side midline

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -301,7 +301,8 @@ public final class Constants {
     public static final String bottomThreeCenterMiddleString = "BottomThreeCenterMiddle";
     public static final String bottomLeaveString = "BottomLeave";
 
-    public static final String threePieceMidlineString = "MidlineThreePiece";
+    public static final String threePieceMidlineSourceString = "MidlineThreePieceSource";
+    public static final String threePieceMidlineAmpString = "MidlineThreePieceAmp";
 
   }
 
